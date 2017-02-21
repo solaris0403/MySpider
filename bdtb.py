29d6c7dba818da04e70917da477d6de4205699b4
caowei <solaris0403@gmail.com>
2017-02-21 15:33:32 +0800

    完善Post

--- bdtb.py
+++ bdtb.py
@@ -35,6 +35,38 @@ class Tool:
         # strip()将前后多余内容删除
         return x.strip()
 
+# 百度贴吧
+class Post(object):
+    def __init__(self, floor=1, author='', time='', content=''):
+        self.__floor = floor
+        self.__author = author
+        self.__time = time
+        self.__content = content
+
+    def getFloor(self):
+        return self.__floor
+
+    def setFloor(self, floor):
+        self.__floor = floor
+
+    def setAuthor(self, author):
+        self.__author = author
+
+    def getAuthor(self):
+        return self.__author
+
+    def setTime(self, time):
+        self.__time = time
+
+    def getTime(self):
+        return self.__time
+
+    def setContent(self, content):
+        self.__content = content
+
+    def getContent(self):
+        return self.__content
+
 
 class BDTB(object):
     def __init__(self, baseUrl, seeLZ=0):
@@ -73,13 +105,20 @@ class BDTB(object):
             return number.get_text()
 
     def getContent(self, page):
+        posts = []
         soup = BeautifulSoup(page, 'lxml')
-        items = soup.find_all('div', class_='d_post_content')
-        print self.tool.replace((items[0]).get_text())
-        # for item in items:
-        #     print(item)
+        items = soup.find_all('div', class_='l_post')
+        for item in items:
+            floor = item.find_all('span', class_='tail-info')[-2].get_text()
+            author = item.find('a', class_='p_author_name').get_text()
+            time = item.find_all('span', class_='tail-info')[-1].get_text()
+            content = self.tool.replace(item.find('div', class_='d_post_content').get_text())
+            post = Post(floor=floor, author=author, time=time, content=content)
+            posts.append(post)
+        return posts
 
 
 baseUrl = 'http://tieba.baidu.com/p/3138733512'
-bdtb = BDTB(baseUrl, 1)
-bdtb.getContent(bdtb.getPage(1))
+bdtb = BDTB(baseUrl, 0)
+for post in bdtb.getContent(bdtb.getPage(1)):
+    print('%s\n%s\t%s\n%s\n' % (post.getFloor(), post.getTime(), post.getAuthor(), post.getContent()))
